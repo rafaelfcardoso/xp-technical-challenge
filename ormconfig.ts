b94ba0a84d59cd4be52ac7f2dd41a235523404de
rafaelfernandescardoso <rafael.fcardoso@hotmail.com>
2025-09-09 16:39:45 -0300

    dev: change database config to use env variables

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,11 +7,10 @@ export default new DataSource({
   port: parseInt(process.env.MYSQL_PORT || '3306'),
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'desafio_xp',
+  database: process.env.MYSQL_DATABASE || 'investment_platform',
   synchronize: false,
   logging: true,
   entities: ['src/entities/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   migrationsTableName: 'migrations',
-  createDatabase: true,
 });
